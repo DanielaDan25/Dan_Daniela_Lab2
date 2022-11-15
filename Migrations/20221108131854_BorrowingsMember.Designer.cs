78e6a60b50a411e7817ea665d7881ec3ad8fab89
Daniela Dan <daniela.dan275@gmail.com>
2022-11-15 12:22:30 +0200

    laborator5

--- Migrations/20221108131854_BorrowingsMember.Designer.cs
+++ Migrations/20221108131854_BorrowingsMember.Designer.cs
@@ -4,6 +4,7 @@ using Dan_Daniela_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dan_Daniela_Lab2.Migrations
 {
     [DbContext(typeof(Dan_Daniela_Lab2Context))]
-    partial class Dan_Daniela_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221108131854_BorrowingsMember")]
+    partial class BorrowingsMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
